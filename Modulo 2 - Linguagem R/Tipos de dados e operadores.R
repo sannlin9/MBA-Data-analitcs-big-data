f756443db562ba3605bed9d3042852e6236144a0
Sandra Lin Costa <sandralin.9@gmail.com>
2023-06-28 21:53:24 -0300

    Create Tipos de dados e operadores.R

---
+++ Modulo 2 - Linguagem R/Tipos de dados e operadores.R
@@ -0,0 +1,33 @@
+##Comentarios
+
+#Declarando variaveis
+variavel <- 'Ola mundo!'
+typeof(variavel)
+class(variavel)
+
+assign('var2','variavel 2')
+
+var3 <- '10'
+typeof(var3)
+class(var3)
+
+#numericos
+
+var4 = 10
+typeof(var4)
+class(var4)
+
+#mudar tipó para inteiro com "L"
+x <-2L
+typeof(x)
+
+#numeros complexos
+
+z = 3 + 4i
+typeof(z)
+
+#boleanos
+
+var1 = TRUE
+var2 = T
+typeof(var1)
